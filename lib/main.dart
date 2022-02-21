877848d2871ec1f8adaf0dd4ed5a2750069a399d
xelahaippa <43220875+xelahaippa@users.noreply.github.com>
2022-02-21 14:44:59 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mest_api_test/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/my_homepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
